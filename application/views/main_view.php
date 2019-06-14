ed6af212bc21d88553d275f9a3eb07acd813bc8d
unknown <itsvincentlouis@gmail.com>
2019-06-14 09:11:09 +0700

    .

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -3,12 +3,57 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <link rel='stylesheet' type='text/css' href='<?= base_url(); ?>assets/css/home.css' />
+<style>
+img{
+    width:210px;
+    height:100px;
+}
+</style>
+    <link rel='stylesheet' type='text/css' href='<?= base_url(); ?>assets/css/home.css?v=0.05' />
 
-    <script src='<?= base_url(); ?>/assets/js/home.js'></script>
+    <script src='<?= base_url(); ?>/assets/js/home.js?v=0.01'></script>
 </head>
 
+
 <body>
-test
+<img src="<?= base_url(); ?>assets/images/site/logo_sipromo_150_maroon.png" />
+
+
+<nav class="navbar">
+<span class="open-slide">
+<a href="#">
+<svg width="30" height="30">
+	<path d="M0,5 30,5" stroke="#fff"
+	stroke-width="5"/>
+	<path d="M0,14 30,14" stroke="#fff"
+	stroke-width="5"/>
+	<path d="M0,23 30,23" stroke="#fff"
+	stroke-width="5"/>
+</svg>
+</a>
+</span>
+
+<ul class="navbar-nav">
+<li><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+</nav>
+<div id="side-menu" class="side-nav">
+<a href="#"class="btn-close" onclick="closeSlideMenu()">&times;</a>
+<a href="#">Home</a>
+<a href="#">About</a>
+<a href="#">Services</a>
+<a href="#">Contact</a>
+</div>
+
+<div id="main">
+	<h1>Menu</h1>
+</div>
+<?= base_url(); ?>
+<script src="home.js">
+</script>
+
 </body>
 </html>
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,13 @@
-alert('a');
+
+
+<script>
+function openSlideMenu(){
+document.getElementById('side-menu').style.width='250px';
+document.getElementById('main').style.marginLeft='250px';
+}
+
+function closeSlideMenu(){
+document.getElementById('side-menu').style.width='0';
+document.getElementById('main').style.marginLeft='0';
+}
+</script>
